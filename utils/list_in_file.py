2905ff7a30ca7bc90d0dd7b2b65d8dea4d5424b9
Aleksey Voko <voko.aleksey@gmail.com>
2020-11-12 10:10:02 +0400

    Fix utils/list_in_file.py

--- utils/list_in_file.py
+++ utils/list_in_file.py
@@ -2,50 +2,31 @@ import pickle
 from pathlib import Path
 
 
-def save_list_to_txt_file(input_list: list, out_file: str, encoding='utf-8'):
-    """
-    сохраняет list в текстовой файл
-    """
+def save_list_to_file(input_list: list, out_file: str, encoding='utf-8'):
     Path(out_file).parent.mkdir(parents=True, exist_ok=True)
     with open(Path(out_file), 'w', encoding=encoding) as f_out:
         for line in input_list:
             f_out.write(str(line) + '\n')
 
 
-def get_eval_list_from_txt_file(input_file: str, encoding='utf-8') -> list:
-    """
-    читает list (eval) из текстового файла
-    """
-    out_list = []
+def get_eval_list_from_file(input_file: str, encoding='utf-8'):
     with open(Path(input_file), encoding=encoding) as f_in:
-        for line in f_in.readlines():
-            out_list.append(eval(line.strip()))
-    return out_list
+        for line in f_in:
+            yield eval(line.rstrip())
 
 
-def get_string_list_from_txt_file(input_file: str, encoding='utf-8') -> list:
-    """
-    читает list из текстового файла построчно
-    """
-    out_list = []
+def get_string_list_from_file(input_file: str, encoding='utf-8'):
     with open(Path(input_file), encoding=encoding) as f_in:
-        for line in f_in.readlines():
-            out_list.append(line.strip())
-    return out_list
+        for line in f_in:
+            yield line.rstrip()
 
 
-def save_object_to_bin_file(input_object, out_file: str):
-    """
-    сохраняет object в бинарный файл
-    """
+def save_object_to_file(in_obj, out_file: str):
     Path(out_file).parent.mkdir(parents=True, exist_ok=True)
     with open(out_file, 'wb') as f_out:
-        pickle.dump(input_object, f_out)
+        pickle.dump(in_obj, f_out)
 
 
-def get_object_from_bin_file(input_file: str):
-    """
-    читает object из бинарного файла
-    """
+def get_object_from_file(input_file: str):
     with open(Path(input_file), 'rb') as f_in:
         return pickle.load(f_in)
