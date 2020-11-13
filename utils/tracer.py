e8a516df8cdf96e1ea7abb078e891b4534941f76
Aleksey Voko <voko.aleksey@gmail.com>
2020-11-13 11:18:39 +0400

    Fix utils/tracer.py

--- utils/tracer.py
+++ utils/tracer.py
@@ -1,5 +1,8 @@
-class Tracer:
-    """Считает количество вызовов декорируемой функции"""
+class Tracer(object):
+    """
+    Считает количество вызовов декорируемой функции.
+    Можно декорировать и функции и методы классов.
+    """
 
     def __init__(self, func):
         self.calls = 0
@@ -8,24 +11,48 @@ class Tracer:
     def __call__(self, *args, **kwargs):
         self.calls += 1
         print(f'call {self.calls} to {self.func.__name__}')
-        self.func(*args, **kwargs)
+        return self.func(*args, **kwargs)
 
+    def __get__(self, instance, owner):
+        def wrapper(*args, **kwargs):
+            return self(instance, *args, **kwargs)
 
-@Tracer
-def spam(a, b, c):
-    print(a + b + c)
-
-
-@Tracer
-def eggs(x, y):
-    print(x ** y)
+        return wrapper
 
 
 if __name__ == '__main__':
+    @Tracer
+    def spam(a, b, c):
+        print(a + b + c)
+
+
+    @Tracer
+    def eggs(N):
+        return 2 ** N
+
+
     spam(1, 2, 3)
     spam(a=4, b=5, c=6)
-    eggs(2, 16)
-    eggs(4, y=4)
+    print(eggs(32))
 
-    print(spam.calls)  # общее количество вызовов отображается, как атрибут декорируемой функции
-    print(spam)
+
+    class Person:
+        def __init__(self, name, pay):
+            self.name = name
+            self.pay = pay
+
+        @Tracer
+        def give_raise(self, percent):
+            self.pay *= (1.0 + percent)
+
+        @Tracer
+        def last_name(self):
+            return self.name.split()[-1]
+
+
+    bob = Person('Bob Smith', 50000)
+    sue = Person('Sue Jones', 100000)
+    print(bob.name, sue.name)
+    sue.give_raise(.10)
+    print(int(sue.pay))
+    print(bob.last_name(), sue.last_name())
