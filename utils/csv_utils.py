ef39a022799839f481462f82910ef8dbe99f2222
Aleksey Voko <voko.aleksey@gmail.com>
2021-01-15 02:06:57 +0400

    Fix utils/csv_utils.py

--- utils/csv_utils.py
+++ utils/csv_utils.py
@@ -4,104 +4,122 @@ from pathlib import Path
 import numpy
 
 
-def save_list_to_csv_file(input_list: list, out_file: str,
-                          encoding='utf-8', newline='', delimiter=','):
+def get_list_of_dicts_from_csv_file(in_f, encoding='utf-8',
+                                    newline='', delimiter=','):
     """
-    сохраняет список списков в csv файл,
-    первый список - строка заголовков
+    Читает список словарей из CSV-файла.
 
-    newline '\r\r\n' -> '\r\n'
-    https://docs.python.org/3/library/csv.html#id3
-
-    delimiter - разделитель [,] or [;]
-
-    docs -> https://docs.python.org/3/library/csv.html
+    :param in_f: CSV-файл
+    :param encoding: кодировка CSV-файла
+    :param newline: newline='' исправляет '\r\r\n' -> '\r\n'
+        https://docs.python.org/3/library/csv.html#id3
+    :param delimiter: разделитель [,] or [;]
+    :return: генератор списка, содержащего словари
     """
-    Path(out_file).parent.mkdir(parents=True, exist_ok=True)
-    with open(Path(out_file), 'w', encoding=encoding, newline=newline) as f_out:
-        csv_writer = csv.writer(f_out, delimiter=delimiter)
-        csv_writer.writerows(input_list)
-
-
-def get_list_of_lists_from_csv_file(input_file: str, encoding='utf-8',
-                                    newline='', delimiter=',') -> list:
-    """
-    читает список списков из csv файла
-    первый список - строка заголовков
-
-    newline '\r\r\n' -> '\r\n'
-    https://docs.python.org/3/library/csv.html#id3
-
-    delimiter - разделитель [,] or [;]
-
-    docs -> https://docs.python.org/3/library/csv.html
-    """
-    out_list = []
-    with open(Path(input_file), encoding=encoding, newline=newline) as f_in:
-        csv_reader = csv.reader(f_in, delimiter=delimiter)
-        for row in csv_reader:
-            out_list.append(row)
-    return out_list
-
-
-def save_list_of_dicts_to_csv_file(input_list: list, out_file: str,
-                                   field_names: list, encoding='utf-8',
-                                   newline='', delimiter=','):
-    """
-    сохраняет список словарей в csv файл,
-    у всех словарей идентичные ключи
-
-    fieldnames - порядок ключей для записи в файл ->
-    fieldnames = ['first_name', 'last_name']
-
-    newline '\r\r\n' -> '\r\n'
-    https://docs.python.org/3/library/csv.html#id3
-
-    delimiter - разделитель [,] or [;]
-
-    docs -> https://docs.python.org/3/library/csv.html
-    """
-    Path(out_file).parent.mkdir(parents=True, exist_ok=True)
-    with open(out_file, 'w', encoding=encoding, newline=newline) as f_out:
-        csv_writer = csv.DictWriter(f_out, fieldnames=field_names, delimiter=delimiter)
-        csv_writer.writeheader()
-        csv_writer.writerows(input_list)
-
-
-def get_list_of_dicts_from_csv_file(input_file: str, encoding='utf-8',
-                                    newline='', delimiter=',') -> list:
-    """
-    читает список словарей из csv файла
-
-    newline='' исправляет '\r\r\n' -> '\r\n'
-    https://docs.python.org/3/library/csv.html#id3
-
-    delimiter - разделитель [,] or [;]
-
-    docs -> https://docs.python.org/3/library/csv.html
-    """
-    out_list = []
-    with open(Path(input_file), encoding=encoding, newline=newline) as f_in:
+    with open(Path(in_f), encoding=encoding, newline=newline) as f_in:
         csv_reader = csv.DictReader(f_in, delimiter=delimiter)
         for row in csv_reader:
-            out_list.append(row)
-    return out_list
+            yield row
 
 
-def get_fieldnames_from_csv_file(input_file: str, encoding='utf-8',
+def save_list_of_dicts_to_csv_file(in_list, out_f, field_names,
+                                   encoding='utf-8', newline='',
+                                   delimiter=','):
+    """
+    Сохраняет список словарей в CSV-файл.
+
+    :param in_list: список, содержащий словари
+    :param out_f: CSV-файл
+    :param field_names: список заголовков колонок CSV-файла
+    :param encoding: кодировка CSV-файла
+    :param newline: newline: newline='' исправляет '\r\r\n' -> '\r\n'
+        https://docs.python.org/3/library/csv.html#id3
+    :param delimiter: разделитель [,] or [;]
+    """
+    Path(out_f).parent.mkdir(parents=True, exist_ok=True)
+    with open(out_f, 'w', encoding=encoding, newline=newline) as f_out:
+        csv_writer = csv.DictWriter(f_out, fieldnames=field_names,
+                                    delimiter=delimiter)
+        csv_writer.writeheader()
+        csv_writer.writerows(in_list)
+
+
+def get_list_of_lists_from_csv_file(in_f, encoding='utf-8',
+                                    newline='', delimiter=','):
+    """
+    Читает список списков из CSV-файла.
+    Первый список - заголовки колонок CSV-файла.
+
+    :param in_f: CSV-файл
+    :param encoding: кодировка CSV-файла
+    :param newline: newline='' исправляет '\r\r\n' -> '\r\n'
+        https://docs.python.org/3/library/csv.html#id3
+    :param delimiter: разделитель [,] or [;]
+    :return: генератор списка, содержащего списки
+    """
+    with open(Path(in_f), encoding=encoding, newline=newline) as f_in:
+        csv_reader = csv.reader(f_in, delimiter=delimiter)
+        for row in csv_reader:
+            yield row
+
+
+def save_list_to_csv_file(in_list, out_f, encoding='utf-8',
+                          newline='', delimiter=','):
+    """
+    Сохраняет список списков в CSV-файл.
+    Первый список - заголовки колонок CSV-файла.
+
+    :param in_list: список, содержащий списки
+    :param out_f: CSV-файл
+    :param encoding: кодировка CSV-файла
+    :param newline: newline: newline='' исправляет '\r\r\n' -> '\r\n'
+        https://docs.python.org/3/library/csv.html#id3
+    :param delimiter: разделитель [,] or [;]
+    """
+    Path(out_f).parent.mkdir(parents=True, exist_ok=True)
+    with open(Path(out_f), 'w', encoding=encoding, newline=newline) as f_out:
+        csv_writer = csv.writer(f_out, delimiter=delimiter)
+        csv_writer.writerows(in_list)
+
+
+def get_fieldnames_from_csv_file(in_f, encoding='utf-8',
                                  newline='', delimiter=','):
-    with open(Path(input_file), encoding=encoding, newline=newline) as f_in:
+    """
+    Возвращает список заголовков колонок CSV-файла.
+
+    :param in_f: CSV-файл
+    :param encoding: кодировка CSV-файла
+    :param newline: newline: newline='' исправляет '\r\r\n' -> '\r\n'
+        https://docs.python.org/3/library/csv.html#id3
+    :param delimiter: разделитель [,] or [;]
+    :return: список заголовков колонок CSV-файла
+    """
+    with open(Path(in_f), encoding=encoding, newline=newline) as f_in:
         return csv.DictReader(f_in, delimiter=delimiter).fieldnames
 
 
-def save_parts_csv(input_file: str, count_parts: int, encoding='utf-8',
+def save_parts_csv(in_f, count_parts, encoding='utf-8',
                    newline='', delimiter=','):
+    """
+    Разделяет CSV-файл на примерно равные части.
+
+    :param in_f: CSV-файл
+    :param count_parts: количество частей
+    :param encoding: кодировка CSV-файла
+    :param newline: newline: newline='' исправляет '\r\r\n' -> '\r\n'
+        https://docs.python.org/3/library/csv.html#id3
+    :param delimiter: разделитель [,] or [;]
+    """
     dict_list = []
-    with open(Path(input_file), encoding=encoding, newline=newline) as f_in:
+    with open(Path(in_f), encoding=encoding, newline=newline) as f_in:
         csv_reader = csv.DictReader(f_in, delimiter=delimiter)
         field_names = list(csv_reader.fieldnames)
         for row in csv_reader:
             dict_list.append(row)
     parts = numpy.array_split(dict_list, count_parts)
     for count, part in enumerate(parts, 1):
-        save_list_of_dicts_to_csv_file(part, f'{input_file}_{count:03}.csv', field_names)
+        save_list_of_dicts_to_csv_file(
+            part,
+            f'{Path(in_f).name}_{count:03}.csv',
+            field_names
+        )
