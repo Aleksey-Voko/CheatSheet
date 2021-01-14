9e865380609632cce28cf9b2edc236fc06b15bb9
Aleksey Voko <voko.aleksey@gmail.com>
2021-01-15 02:22:01 +0400

    Fix utils/list_in_file.py

--- utils/list_in_file.py
+++ utils/list_in_file.py
@@ -2,31 +2,31 @@ import pickle
 from pathlib import Path
 
 
-def save_list_to_file(input_list: list, out_file: str, encoding='utf-8'):
-    Path(out_file).parent.mkdir(parents=True, exist_ok=True)
-    with open(Path(out_file), 'w', encoding=encoding) as f_out:
-        for line in input_list:
-            f_out.write(str(line) + '\n')
-
-
-def get_eval_list_from_file(input_file: str, encoding='utf-8'):
-    with open(Path(input_file), encoding=encoding) as f_in:
-        for line in f_in:
-            yield eval(line.rstrip())
-
-
-def get_string_list_from_file(input_file: str, encoding='utf-8'):
-    with open(Path(input_file), encoding=encoding) as f_in:
+def get_str_list_from_file(in_f, encoding='utf-8'):
+    with open(Path(in_f), encoding=encoding) as f_in:
         for line in f_in:
             yield line.rstrip()
 
 
-def save_object_to_file(in_obj, out_file: str):
-    Path(out_file).parent.mkdir(parents=True, exist_ok=True)
-    with open(out_file, 'wb') as f_out:
+def get_eval_list_from_file(in_f, encoding='utf-8'):
+    with open(Path(in_f), encoding=encoding) as f_in:
+        for line in f_in:
+            yield eval(line.rstrip())
+
+
+def save_str_list_to_file(in_list, out_f, encoding='utf-8'):
+    Path(out_f).parent.mkdir(parents=True, exist_ok=True)
+    with open(Path(out_f), 'w', encoding=encoding) as f_out:
+        for line in in_list:
+            f_out.write(str(line) + '\n')
+
+
+def save_obj_to_file(in_obj, out_f):
+    Path(out_f).parent.mkdir(parents=True, exist_ok=True)
+    with open(out_f, 'wb') as f_out:
         pickle.dump(in_obj, f_out)
 
 
-def get_object_from_file(input_file: str):
-    with open(Path(input_file), 'rb') as f_in:
+def get_obj_from_file(in_f):
+    with open(Path(in_f), 'rb') as f_in:
         return pickle.load(f_in)
